ef87b6999d0dfefc3fcd6d255d4b30183ff3ccb8
Valentina <valenprofitos2001@gmail.com>
2023-12-03 17:32:14 -0300

    Update file

--- 01. Introduction/002.Java Stdin and Stdout i/Solution.java
+++ 01. Introduction/002.Java Stdin and Stdout i/Solution.java
@@ -3,19 +3,20 @@ import java.util.*;
 public class Solution {
     
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        try (Scanner scan = new Scanner(System.in)) {
+            int a = scan.nextInt();
+            // Complete this line
+            // Complete this line
 
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
 
-        System.out.println(a);
-        // Complete this line
-        // Complete this line
-        
-        System.out.println(b);
-        System.out.println(c);
+            System.out.println(a);
+            // Complete this line
+            // Complete this line
+            
+            System.out.println(b);
+            System.out.println(c);
+        }
     }
 }
